cb6333da0e99440b1004f400a77ca2d8322540bb
Alex Oliinyk <alex.p.oliinyk@gmail.com>
2023-05-31 18:10:36 +0200

    Implemented rna-transcription exercise.

---
+++ rna-transcription/rna_transcription.pl
@@ -0,0 +1,9 @@
+transcript('G', 'C') :- !.
+transcript('C', 'G') :- !.
+transcript('T', 'A') :- !.
+transcript('A', 'U') :- !.
+
+rna_transcription(Rna, Dna) :- 
+    string_chars(Rna, Dnucleotides),
+    maplist(transcript, Dnucleotides, Rnucleotides),
+    string_chars(Dna, Rnucleotides).
